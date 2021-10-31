ec7e4334d63b10e82d1b5afc6cff0a5a09cc68f5
Mahi Singh Rawat <mahisnghrwt@gmail.com>
2021-11-01 04:07:09 +1100

    change production api url

--- src/settings.js
+++ src/settings.js
@@ -1,9 +1,6 @@
 const settings = {
-	WEBSOCKET_HOST: "wss://localhost:44370",
-	// API_ROOT: "http://localhost:80/api/v1",
-	__API_ROOT: "https://localhost:443/api/v1",
-	API_ROOT: process.env.NODE_ENV === 'production' ? 'https://143.198.208.237:44343/api/v1' : "https://localhost:44370/api/v1",
-	SIGNAL_R_URL: process.env.NODE_ENV === 'production' ? 'https://143.198.208.237:44343/notification' : "https://localhost:44343/notification",
+	API_ROOT: process.env.NODE_ENV === 'production' ? 'https://echobash.com/api/v1' : "https://localhost:44370/api/v1",
+	SIGNAL_R_URL: process.env.NODE_ENV === 'production' ? 'https://echobash.com/notification' : "https://localhost:44370/notification",
 	getProjectUrl: function(projectId) {
 		if (!projectId) return this.API_ROOT + "/project"
 		return `${settings.API_ROOT}/project/${projectId}`;
